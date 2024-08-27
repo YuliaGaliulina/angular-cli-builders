436c7ccc5209d1ff22d2115eff8b7e04dcf5a22e
Yulia Galiulina <92juliegal@gmail.com>
2024-08-27 16:17:08 +0300

    Add schema path for application in v18

--- src/app/services/builder-http.service.ts
+++ src/app/services/builder-http.service.ts
@@ -6,7 +6,8 @@ import { Builder } from "../state/builder-list/Builder";
 import { dereference, JSONSchema } from "@apidevtools/json-schema-ref-parser";
 import { SelectedBuilder } from "../state/selected-builder/selected-builder.store";
 
-const baseUrl = 'https://unpkg.com/@angular-devkit/build-angular@';
+const ANGULAR_DEVKIT_BUILD = 'https://unpkg.com/@angular-devkit/build-angular@';
+const ANGULAR_BUILD = 'https://unpkg.com/@angular/build@';
 
 @Injectable({
     providedIn: 'root'
@@ -17,16 +18,29 @@ export class BuilderHttpService {
     }
     
     getBuilders(version: NgVersion): Observable<Builder[]> {
-        return this.http.get<{ builders: any }>(`${baseUrl}${version.version}/builders.json`)
+        return this.http.get<{ builders: any }>(`${ANGULAR_DEVKIT_BUILD}${version.version}/builders.json`)
             .pipe(map((resp) => {
+                const cliVersion = +version.majorVersion.split('v')[1];
                 const builders = resp.builders;
-                return Object.keys(builders).map(key => ({ title: key, schemaUrl: builders[key].schema }))
+                
+                return Object.keys(builders).map(key => {
+                    let schemaPath = '';
+                    
+                    if (cliVersion > 17 && key === 'application') {
+                        schemaPath = `${ANGULAR_BUILD}ngVersion/src/builders/application/schema.json`;
+                    } else {
+                        const subPath = builders[key].schema.replace(/^\.\//, '');
+                        schemaPath = `${ANGULAR_DEVKIT_BUILD}ngVersion/${subPath}`;
+                    }
+                    
+                    return { title: key, schemaUrl: schemaPath }
+                })
             }))
     }
     
     getBuilder(version: string, builder: Builder): Observable<SelectedBuilder> {
-        const formattedPath = builder.schemaUrl.replace(/^\.\//, '');
-        return this.http.get<JSONSchema>(`${baseUrl}${version}/${formattedPath}`)
+        const schemaPath = builder.schemaUrl.replace('ngVersion', version);
+        return this.http.get<JSONSchema>(schemaPath)
             .pipe(
                 map((schema) => {
                     if (schema.$id) {
@@ -36,7 +50,7 @@ export class BuilderHttpService {
                     return schema;
                 }),
                 switchMap((schema) => from(dereference(schema))),
-                map((schema) => ({title: builder.title, schema}))
+                map((schema) => ({ title: builder.title, schema }))
             );
     }
 }
