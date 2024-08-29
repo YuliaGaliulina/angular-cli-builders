8756b9485d602371572b46538891000552667712
Yulia Galiulina <92juliegal@gmail.com>
2024-08-29 15:04:24 +0300

    Fix leftover old project names

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-config-docs' title`, () => {
+  it(`should have the 'angular-cli-builders' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-cli-builders');
