7b76413e25e6aca4854ac7be4082961b17a6d221
Yulia Galiulina <92juliegal@gmail.com>
2024-11-21 22:18:33 +0300

    remove zone.js

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,4 +1,7 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+    ApplicationConfig,
+    provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -6,9 +9,9 @@ import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
         provideAnimationsAsync(),
         provideHttpClient(),
+        provideExperimentalZonelessChangeDetection()
     ]
 };
